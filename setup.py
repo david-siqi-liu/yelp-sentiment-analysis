9ab0461f3af758382da351ea123b810ebf4c43f5
David Liu <davidsiqiliu@gmail.com>
2020-02-17 10:22:13 -0500

    Started cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Empirical analysis of various ML/AL algorithms on text classification task on the Yelp review dataset',
+    author='David Liu',
+    license='MIT',
+)
